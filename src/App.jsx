c3ce3febb9e0b4b48c6a4c10c280fd83c24e0c63
Tushar Parwani <167443525+Tushar-9-9@users.noreply.github.com>
2024-07-07 19:46:51 +0530

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -9,24 +8,16 @@ import Header from './components/Header';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter>   
     <Header />
+
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/about' element={<About />} />
-      <Route path='/search' element={<Search />} />
-      <Route path='/listing/:listingId' element={<Listing />} />
-
-      <Route element={<PrivateRoute />}>
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/create-listing' element={<CreateListing />} />
-        <Route
-          path='/update-listing/:listingId'
-          element={<UpdateListing />}
-        />
-      </Route>
+      <Route path='/profile' element={<Profile />} />
+      
     </Routes>
   </BrowserRouter>
   )
