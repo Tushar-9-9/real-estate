46f2e7b11e8778f3b557c6fb1e6b5eb16a8e4457
Tushar Parwani <167443525+Tushar-9-9@users.noreply.github.com>
2024-07-08 18:38:04 +0530

    created test api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 
 dotenv.config();
 
-const app=express();
+
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,6 +17,10 @@ mongoose
     console.log(err);
   });
 
+const app=express();
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
+app.use("/api/user",userRouter)
