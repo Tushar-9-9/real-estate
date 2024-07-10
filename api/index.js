e90d11f2426de4faa20f9225edcba99166466364
Tushar Parwani <167443525+Tushar-9-9@users.noreply.github.com>
2024-07-11 03:24:16 +0530

    update user functionality

--- api/index.js
+++ api/index.js
@@ -3,11 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
-
-
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,17 +14,17 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const app = express();
 
-const app=express();
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
-
-app.use("/api/user",userRouter);
-app.use("/api/auth", authRouter);
-
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
