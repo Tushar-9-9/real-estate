6b33a1f276c4c0556266be310d814a6ac1edb377
Tushar Parwani <167443525+Tushar-9-9@users.noreply.github.com>
2024-07-14 21:21:35 +0530

    search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import Search from './pages/Search';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
@@ -20,6 +21,7 @@ const App = () => {
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/about' element={<About />} />
+      <Route path='/search' element={<Search />} />
       <Route path='/listing/:listingId' element={<Listing />} />
       <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,9 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
+
 export default function Header() {
+
   const { currentUser } = useSelector((state) => state.user);
   const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
@@ -14,7 +16,6 @@ export default function Header() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
@@ -22,14 +23,14 @@ export default function Header() {
       setSearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
-  return (
-    <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
 
+  return (
+    <header className='bg-blue-700 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-white'>NuttyToe</span>
+            <span className='text-white'>Estate</span>
           </h1>
         </Link>
         <form
@@ -50,10 +51,11 @@ export default function Header() {
         <ul className='flex gap-4'>
           <Link to='/'>
 
-  
+
             <li className='hidden sm:inline text-slate-700 hover:underline'>
               Home
             </li>
+
           </Link>
           <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
@@ -63,6 +65,7 @@ export default function Header() {
           <Link to='/profile'>
             {currentUser ? (
               <img
+
                 className='rounded-full h-7 w-7 object-cover'
                 src={currentUser.avatar}
                 alt='profile'
@@ -76,5 +79,8 @@ export default function Header() {
         </ul>
       </div>
     </header>
+
+
+  
   );
 }
