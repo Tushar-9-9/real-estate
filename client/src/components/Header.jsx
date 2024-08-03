969fbeca6e0d5e6d5676357fe5085dc06cda7b11
Tushar Parwani <167443525+Tushar-9-9@users.noreply.github.com>
2024-08-04 05:28:07 +0530

    Styling changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,8 +29,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-white'>NuttyToe</span>
-            <span className='text-white'>Estate</span>
+            <span className='text-white'>My</span>
+            <span className='text-white'>Place</span>
           </h1>
         </Link>
         <form
